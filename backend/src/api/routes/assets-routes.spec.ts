3461029e285d36bdfa26b3c32bc08731c0810bf0
Daniel Cruz <danielcunha54@gmail.com>
2022-09-01 18:49:17 -0300

    feat: add route to query assets

---
+++ backend/src/api/routes/assets-routes.spec.ts
@@ -0,0 +1,45 @@
+import request from 'supertest'
+
+import { PrismaClient } from '@prisma/client'
+
+import { makeAsset } from '../../../tests/factories'
+import prisma from '../../../tests/prisma'
+import { api } from '../index'
+
+let client: PrismaClient
+
+const makeSut = () => {
+  return request(api)
+}
+
+describe('/assets', () => {
+  beforeAll(async () => {
+    client = await prisma.connect()
+  })
+
+  beforeEach(async () => {
+    await client.asset.deleteMany()
+  })
+
+  afterAll(async () => {
+    await prisma.disconnect()
+  })
+
+  describe('GET /', () => {
+    const assets = [
+      makeAsset({ name: 'any-asset' }),
+      makeAsset({ name: 'other-asset' }),
+      makeAsset({ name: 'random-name' })
+    ]
+
+    it('searches available assets', async () => {
+      await client.asset.createMany({ data: assets })
+      await makeSut().get('/assets').expect(200, assets)
+    })
+
+    it('filters available assets', async () => {
+      await client.asset.createMany({ data: assets })
+      await makeSut().get('/assets?query=asset').expect(200, [assets[0], assets[1]])
+    })
+  })
+})
--- backend/src/api/routes/assets-routes.ts
+++ backend/src/api/routes/assets-routes.ts
@@ -1,3 +1,14 @@
 import { Router } from 'express'
 
+import { container } from '../../config/container'
+import { ISearchAssets } from '../../core/services/assets/search-assets'
+
 export const routes = Router()
+
+routes.get('/', async (request, response) => {
+  const query = request.query.query?.toString() || ''
+  const service = container.get(ISearchAssets)
+  const assets = await service.execute(query)
+
+  return response.json(assets)
+})
