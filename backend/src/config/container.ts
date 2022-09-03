74a8bea5cc0cdb0cd471519165bc15366ebdf929
Daniel Cruz <danielcunha54@gmail.com>
2022-09-03 14:18:26 -0300

    chore: automatically load container modules

--- backend/src/config/container.ts
+++ backend/src/config/container.ts
@@ -1,16 +1,38 @@
+import { readdir } from 'fs/promises'
 import { Container } from 'inversify'
 import { buildProviderModule } from 'inversify-binding-decorators'
+import { resolve } from 'path'
 
 import { PrismaClient } from '@prisma/client'
 
-// TODO: Automatically import dependencies, perhaps using a custom decorator
-import '../database/repositories'
-import '../core/services/assets/search-assets'
-import '../core/services/brokers/list-brokers'
-import '../core/services/transactions/create-transaction'
-import '../core/services/transactions/list-transactions'
 import { client } from '../database/connection'
 
 export const container = new Container()
-container.load(buildProviderModule())
-container.bind(PrismaClient).toConstantValue(client)
+
+async function importModules() {
+  // Import all repositories from indexes file
+  await import('../database/repositories')
+
+  // Import all services from subfolders
+  const basePath = resolve(__dirname, '..', 'core', 'services')
+  const subfolders = await readdir(basePath)
+  const filePaths: string[] = []
+
+  for (const folder of subfolders) {
+    const folderPath = resolve(basePath, folder)
+    const files = await readdir(folderPath)
+    files
+      .filter(file => !file.endsWith('.spec.ts'))
+      .forEach(file => filePaths.push(resolve(folderPath, file)))
+  }
+
+  await Promise.all(filePaths.map(filepath => import(filepath)))
+}
+
+async function setup() {
+  await importModules()
+  container.load(buildProviderModule())
+  container.bind(PrismaClient).toConstantValue(client)
+}
+
+setup()
